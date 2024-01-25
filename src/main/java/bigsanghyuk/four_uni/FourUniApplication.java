b9feae19171415b2a8a6a18071bdb03d7c46110d
JoJeHun <cho9759@gmail.com>
2024-01-25 09:57:54 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/bigsanghyuk/four_uni/FourUniApplication.java
@@ -0,0 +1,13 @@
+package bigsanghyuk.four_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourUniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bigsanghyuk/four_uni/FourUniApplicationTests.java
@@ -0,0 +1,13 @@
+package bigsanghyuk.four_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
