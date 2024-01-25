b0a9043d9998eed0b1fc76438d8e280cc8869ed6
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-01-25 10:50:02 +0900

    ✨ 기능 추가 / 수정: Report 엔티티 추가

---
+++ src/main/java/bigsanghyuk/four_uni/entity/Report.java
@@ -0,0 +1,30 @@
+package bigsanghyuk.four_uni.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long targetId;
+    private int targetType;
+    private Long userId;
+    private Long targetUserId;
+    private int reasonId;
+    private String detail;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
