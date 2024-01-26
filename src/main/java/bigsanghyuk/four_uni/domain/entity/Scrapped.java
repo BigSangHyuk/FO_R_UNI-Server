549e1ccdd2705b236a722d7222c32ea53a23eab0
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-01-26 21:15:48 +0900

    ♻️ 코드 수정: Scrapped 생성자 추가

--- src/main/java/bigsanghyuk/four_uni/domain/entity/Scrapped.java
+++ src/main/java/bigsanghyuk/four_uni/domain/entity/Scrapped.java
@@ -30,4 +30,14 @@ public class Scrapped {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    public Scrapped(Long userId, Long postId, Long categoryId) {
+        this.userId = userId;
+        this.postId = postId;
+        this.categoryId = categoryId;
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.scrappedAt = now;  //scrap 해제 후 다시 scrap 상황 생각시
+        this.updatedAt = now;   //삭제 후 새로운 컬럼 등록할 것 같아 생성자에 설정
+    }
 }
