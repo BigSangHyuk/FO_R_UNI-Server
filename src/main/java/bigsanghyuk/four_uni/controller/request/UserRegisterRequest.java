cb34173e4c9a93cca2032c0a223e2e3c50ada8c3
JoJeHun <cho9759@gmail.com>
2024-01-26 14:50:08 +0900

    ✨ 기능 추가 / 수정 - 회원가입 Request, Response 추가

---
+++ src/main/java/bigsanghyuk/four_uni/controller/request/UserRegisterRequest.java
@@ -0,0 +1,29 @@
+package bigsanghyuk.four_uni.controller.request;
+
+
+import bigsanghyuk.four_uni.domain.UserRegisterInfo;
+import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+public class UserRegisterRequest {
+
+    @NotNull
+    private Long id;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+    @NotNull
+    private int dept;
+    @NotNull
+    private String nickName;
+
+    private String image;
+
+    public UserRegisterInfo toDomain() {
+        return new UserRegisterInfo(id, email, password, name, dept, nickName, image);
+    }
+}
---
+++ src/main/java/bigsanghyuk/four_uni/controller/response/CommonResponse.java
@@ -0,0 +1,11 @@
+package bigsanghyuk.four_uni.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CommonResponse {
+    private boolean isSuccess;
+
+}
