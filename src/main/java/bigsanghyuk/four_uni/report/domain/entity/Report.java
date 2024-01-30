e695b78e4be69f58dc63741df49ff7cc12d8ff2f
JoJeHun <cho9759@gmail.com>
2024-01-30 16:41:16 +0900

    ✨ 기능 추가 - report : 댓글 신고 기능 (컨트롤러, DTO 추가, 엔티티 내용 추가)

--- src/main/java/bigsanghyuk/four_uni/report/domain/entity/Report.java
+++ src/main/java/bigsanghyuk/four_uni/report/domain/entity/Report.java
@@ -1,5 +1,8 @@
 package bigsanghyuk.four_uni.report.domain.entity;
 
+import bigsanghyuk.four_uni.comment.domain.entity.Comment;
+import bigsanghyuk.four_uni.exception.ReportReason;
+import bigsanghyuk.four_uni.user.domain.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -7,19 +10,28 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-@Getter @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Report {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long targetId;
-    private int targetType;
-    private Long userId;
-    private Long targetUserId;
-    private int reasonId;
-    private String detail;
+//    private Long targetId; // 신고되는 댓글 or 게시글의 id
+//    private int targetType; // 댓글인지 게시글인지
+
+    @ManyToOne
+    private User user; // 신고하는 유저의 아이디
+
+    @ManyToOne
+    private Comment comment;
+
+
+    @Enumerated(EnumType.STRING)
+    private ReportReason reason; // 신고 사유
+
+//    private String detail; // 상세 사유
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
