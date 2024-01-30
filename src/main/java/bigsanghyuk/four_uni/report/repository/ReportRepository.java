bd82667ae14bdc25a3bbbdb6aa13b833ab746f1f
JoJeHun <cho9759@gmail.com>
2024-01-30 16:42:01 +0900

    ✨ 기능 추가 - report : 댓글 신고 기능 (신고 사유 ENUM 클래스 추가, 리포지토리 추가)

---
+++ src/main/java/bigsanghyuk/four_uni/report/repository/ReportRepository.java
@@ -0,0 +1,13 @@
+package bigsanghyuk.four_uni.report.repository;
+
+import bigsanghyuk.four_uni.comment.domain.entity.Comment;
+import bigsanghyuk.four_uni.report.domain.entity.Report;
+import bigsanghyuk.four_uni.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    Optional<Report> findByUserAndComment(User user, Comment comment);
+}
