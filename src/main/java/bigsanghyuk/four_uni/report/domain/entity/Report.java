39b1ede025a9b08b9034f047690d045d9126b722
JoJeHun <cho9759@gmail.com>
2024-01-31 19:02:31 +0900

    ♻️ 코드 수정 - Report 엔티티 수정

--- src/main/java/bigsanghyuk/four_uni/report/domain/entity/Report.java
+++ src/main/java/bigsanghyuk/four_uni/report/domain/entity/Report.java
@@ -3,7 +3,13 @@ package bigsanghyuk.four_uni.report.domain.entity;
 import bigsanghyuk.four_uni.comment.domain.entity.Comment;
 import bigsanghyuk.four_uni.exception.ReportReason;
 import bigsanghyuk.four_uni.user.domain.entity.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -18,13 +24,11 @@ public class Report {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    private Long targetId; // 신고되는 댓글 or 게시글의 id
-//    private int targetType; // 댓글인지 게시글인지
-
     private Long userId; // 신고하는 유저의 아이디
 
     private Long commentId;
 
+    private Long postId;
 
     @Enumerated(EnumType.STRING)
     private ReportReason reason; // 신고 사유
@@ -37,4 +41,7 @@ public class Report {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+//    private Long targetId; // 신고되는 댓글 or 게시글의 id
+//    private int targetType; // 댓글인지 게시글인지
 }
