ff69cbbc27dc33b82f47e6ce84a125e4948ae9bf
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-01-31 10:46:23 +0900

    ✨ 기능 추가 / 수정: 유저 정보 업데이트 시 비밀번호 해싱

--- src/main/java/bigsanghyuk/four_uni/user/dto/request/UpdateUserRequest.java
+++ src/main/java/bigsanghyuk/four_uni/user/dto/request/UpdateUserRequest.java
@@ -3,10 +3,14 @@ package bigsanghyuk.four_uni.user.dto.request;
 import bigsanghyuk.four_uni.user.domain.UpdateUserInfo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @NoArgsConstructor
 public class UpdateUserRequest {
+
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
 
     private Long id;
     private String password;
@@ -14,7 +18,7 @@ public class UpdateUserRequest {
     private String image;
 
     public UpdateUserInfo toDomain() {
-        return new UpdateUserInfo(id, password, nickName, image);
+        return new UpdateUserInfo(id, encoder.encode(password), nickName, image);
     }
 
 }
