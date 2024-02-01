5fb31e2d17969f074480d4e34efe1f20a6403584
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-01 10:17:45 +0900

    ♻️ 코드 수정: 반환 타입 변경

--- src/main/java/bigsanghyuk/four_uni/post/controller/PostController.java
+++ src/main/java/bigsanghyuk/four_uni/post/controller/PostController.java
@@ -5,12 +5,16 @@ import bigsanghyuk.four_uni.post.domain.entity.Post;
 import bigsanghyuk.four_uni.post.dto.response.GetPostResponse;
 import bigsanghyuk.four_uni.post.service.PostService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +24,7 @@ public class PostController {
 
     // 게시글 단 건 조회
     @GetMapping("/v1/posts/{postId}")
-    public ResponseEntity<CommonResponse> getDetail(@PathVariable Long postId) {
+    public ResponseEntity<GetPostResponse> getDetail(@PathVariable Long postId) {
         Post post = postService.getDetail(postId);
 
         GetPostResponse getPostResponse = GetPostResponse.builder()
@@ -34,12 +38,25 @@ public class PostController {
     }
 
     @GetMapping("/v1/posts/unclassified")
-    public List<Post> getUnclassified() {
-        return postService.getUnClassifiedLists();
+    public ResponseEntity<Result<List<Post>>> getUnclassified() {
+        List<Post> unClassified = postService.getUnClassifiedLists();
+        return ResponseEntity.ok().body(new Result<>(unClassified, unClassified.size()));
     }
 
     @GetMapping("/v1/posts/filter")
-    public List<Post> getByFiltered(@Valid @RequestBody List<Long> categoryIds) {
-        return postService.getFilteredPosts(categoryIds);
+    public ResponseEntity<Result<List<Post>>> getByFiltered(@Valid @RequestBody List<Long> categoryIds) {
+        List<Post> filtered = postService.getFilteredPosts(categoryIds);
+        return ResponseEntity.ok().body(new Result<>(filtered, filtered.size()));
+    }
+
+    @Getter @Setter
+    public static class Result<T> {
+        private T data;
+        private int count;
+
+        public Result(T data, int count) {
+            this.data = data;
+            this.count = count;
+        }
     }
 }
