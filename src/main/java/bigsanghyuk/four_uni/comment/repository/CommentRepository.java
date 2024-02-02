0bd1ab104ecfad00893ae955077f74a6665c62f9
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-02 22:33:35 +0900

    ♻️ 코드 수정: repository 메소드 어노테이션 추가

--- src/main/java/bigsanghyuk/four_uni/comment/repository/CommentRepository.java
+++ src/main/java/bigsanghyuk/four_uni/comment/repository/CommentRepository.java
@@ -2,8 +2,10 @@ package bigsanghyuk.four_uni.comment.repository;
 
 import bigsanghyuk.four_uni.comment.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,7 +13,8 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     void deleteCommentByPostIdAndId(Long postId, Long commentId);
 
-    //@Transactional
+    @Transactional
+    @Modifying
     @Query("UPDATE Comment c SET c.commentLike = c.commentLike + 1 WHERE c.id = :commentId")
     void incrementLikesByCommentId(@Param("commentId") Long commentId);
 
