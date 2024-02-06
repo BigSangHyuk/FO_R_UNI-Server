be0e9eae344a95ead490575e8596c176e03a753d
JoJeHun <cho9759@gmail.com>
2024-02-06 10:47:01 +0900

    ✨ 기능 추가 - Post 예외처리 추가

--- src/main/java/bigsanghyuk/four_uni/exception/post/PostNotFoundException.java
+++ src/main/java/bigsanghyuk/four_uni/exception/post/PostNotFoundException.java
@@ -8,7 +8,7 @@ public class PostNotFoundException extends IllegalArgumentException {
 
     private final StatusEnum status;
 
-    private static final String message = "해당 게시글이 더 이상 존재하지 않습니다. 다시 한번 확인하세요.";
+    private static final String message = "해당 게시글이 더 이상 존재하지 않습니다.";
 
     public PostNotFoundException() {
         super(message);
