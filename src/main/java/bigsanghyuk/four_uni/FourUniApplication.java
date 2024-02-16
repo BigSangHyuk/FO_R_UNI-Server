cd9b8922274b46d1d149e07a7c6ac90bd221ba8c
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-16 23:59:14 +0900

    ♻️ 코드 수정: createdAt 자동 할당을 위한 JpaAuditing 허용

--- src/main/java/bigsanghyuk/four_uni/FourUniApplication.java
+++ src/main/java/bigsanghyuk/four_uni/FourUniApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
 public class FourUniApplication {
 
     public static void main(String[] args) {
