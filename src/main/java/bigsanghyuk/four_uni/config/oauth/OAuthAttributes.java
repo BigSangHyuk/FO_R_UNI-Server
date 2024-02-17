d0089d9ad5bd3a536eced7856525f148f45129c7
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-17 17:46:15 +0900

    ♻️ 코드 수정: oauth 속성 값 제공 클래스 수정

--- src/main/java/bigsanghyuk/four_uni/config/oauth/OAuthAttributes.java
+++ src/main/java/bigsanghyuk/four_uni/config/oauth/OAuthAttributes.java
@@ -25,13 +25,13 @@ public class OAuthAttributes {
         this.image = image;
     }
 
-    public static OAuthAttributes of(String registrationId, String nameAttributeKey, Map<String, Object> attributes) {
+    public static OAuthAttributes of(String registrationId, Map<String, Object> attributes) {
         if ("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         } else if ("kakao".equals(registrationId)) {
             return ofKakao("id", attributes);
         } else {
-            return ofGoogle(nameAttributeKey, attributes);
+            return ofGoogle("sub", attributes);
         }
     }
 
@@ -39,10 +39,10 @@ public class OAuthAttributes {
         return OAuthAttributes.builder()
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
-                .name((String) attributes.get("fullName"))
+                .name((String) attributes.get("name"))
                 .nickName((String) attributes.get("displayName"))
                 .email((String) attributes.get("email"))
-                .image((String) attributes.get("photoUrl"))
+                .image((String) attributes.get("picture"))
                 .build();
     }
 
@@ -59,13 +59,15 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
         return OAuthAttributes.builder()
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
-                .nickName((String) attributes.get("profile_nickname"))
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("account_email"))
-                .image((String) attributes.get("profile_image"))
+                .nickName((String) kakaoProfile.get("nickname"))
+                .name((String) kakaoAccount.get("name"))
+                .email((String) kakaoAccount.get("email"))
+                .image((String) kakaoProfile.get("profile_image_url"))
                 .build();
     }
 }
