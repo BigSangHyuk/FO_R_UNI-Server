a3e531cafe5b88db2816586913ebe4ae732a60d2
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-19 20:43:23 +0900

    ♻️ 코드 수정: 서버 시작시 추가되는 예제 데이터 삭제

--- src/main/java/bigsanghyuk/four_uni/FourUniApplication.java
+++ src/main/java/bigsanghyuk/four_uni/FourUniApplication.java
@@ -1,13 +1,9 @@
 package bigsanghyuk.four_uni;
 
-import bigsanghyuk.four_uni.post.repository.PostRepository;
-import bigsanghyuk.four_uni.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
@@ -15,11 +11,6 @@ public class FourUniApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FourUniApplication.class, args);
-    }
-
-    @Bean
-    public ExampleData testDataInit(UserRepository userRepository, PostRepository postRepository, PasswordEncoder encoder) {
-        return new ExampleData(userRepository, postRepository, encoder);
     }
 
 }
