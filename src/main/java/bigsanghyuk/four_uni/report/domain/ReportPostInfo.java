2358dd3c98ff727ae03aac432ef522d39a7ddfb6
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-02-27 10:19:25 +0900

    ✨ 기능 추가: 신고 기능 dto 추가

---
+++ src/main/java/bigsanghyuk/four_uni/report/domain/ReportPostInfo.java
@@ -0,0 +1,15 @@
+package bigsanghyuk.four_uni.report.domain;
+
+import bigsanghyuk.four_uni.report.domain.entity.ReportReason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReportPostInfo {
+
+    private Long userId;
+    private Long postId;
+    private ReportReason reason;
+    private String detail;
+}
