72822a1de984b3acee4bc2b627c07c23a80cc703
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-19 14:09:46 +0900

    ♻️ 코드 수정: 엔티티 조건 변경으로 인한 코드 수정

--- src/main/java/bigsanghyuk/four_uni/post/dto/response/GetDetailResponse.java
+++ src/main/java/bigsanghyuk/four_uni/post/dto/response/GetDetailResponse.java
@@ -3,7 +3,7 @@ package bigsanghyuk.four_uni.post.dto.response;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -15,7 +15,7 @@ public class GetDetailResponse {
     private Long categoryId;
     private String title;
     private String content;
-    private String imageUrl;
+    private List<String> imageUrl;
     private int views;
     private boolean isClassified;
     private LocalDate postedAt;
