0412787007876727af0513b582decbfeafffdd01
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-25 10:57:59 +0900

    ♻️ 코드 수정: cors 설정 수정

--- src/main/java/bigsanghyuk/four_uni/config/CorsFilter.java
+++ src/main/java/bigsanghyuk/four_uni/config/CorsFilter.java
@@ -23,6 +23,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:19006");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
