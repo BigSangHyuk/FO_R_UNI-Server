4037e62b9a1c6db66478762c97d8829999d0f3e4
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-25 10:59:17 +0900

    ✨ 기능 추가: 임시 비밀번호 발급 후 변경 적용

--- src/main/java/bigsanghyuk/four_uni/user/repository/UserRepository.java
+++ src/main/java/bigsanghyuk/four_uni/user/repository/UserRepository.java
@@ -2,6 +2,10 @@ package bigsanghyuk.four_uni.user.repository;
 
 import bigsanghyuk.four_uni.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByEmailAndNickName(String email, String nickName);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.password = :password WHERE u.email = :email")
+    void updatePassword(@Param("email") String email, @Param("password") String encodedPw);
 }
--- src/main/java/bigsanghyuk/four_uni/user/service/UserService.java
+++ src/main/java/bigsanghyuk/four_uni/user/service/UserService.java
@@ -4,9 +4,12 @@ import bigsanghyuk.four_uni.config.jwt.JwtProvider;
 import bigsanghyuk.four_uni.config.jwt.domain.Token;
 import bigsanghyuk.four_uni.config.jwt.domain.TokenRepository;
 import bigsanghyuk.four_uni.config.jwt.dto.TokenDto;
+import bigsanghyuk.four_uni.config.mail.domain.SendMailInfo;
+import bigsanghyuk.four_uni.config.mail.service.MailService;
 import bigsanghyuk.four_uni.exception.jwt.TokenNotFoundException;
 import bigsanghyuk.four_uni.exception.user.EmailDuplicateException;
 import bigsanghyuk.four_uni.exception.user.UserNotFoundException;
+import bigsanghyuk.four_uni.user.domain.ChangePasswordInfo;
 import bigsanghyuk.four_uni.user.domain.EditUserInfo;
 import bigsanghyuk.four_uni.user.domain.LoginUserInfo;
 import bigsanghyuk.four_uni.user.domain.SignUserInfo;
@@ -35,9 +38,11 @@ public class UserService {
     private final PasswordEncoder encoder;
     private final JwtProvider jwtProvider;
     private final TokenRepository tokenRepository;
+    private final MailService mailService;
 
     private static final int EXPIRATION_IN_MINUTES = 43800;
 
+    // 회원 가입
     public boolean register(SignUserInfo info) throws Exception {
         userRepository.findByEmail(info.getEmail())
                 .ifPresent(user -> {
@@ -64,6 +69,7 @@ public class UserService {
         return true;
     }
 
+    // 회원 정보 수정
     @Transactional
     public EditResponse edit(Long userId, EditUserInfo info) {
         User user = userRepository.findById(userId)
@@ -80,6 +86,7 @@ public class UserService {
                 .build();
     }
 
+    // 로그인
     public LoginResponse login(LoginUserInfo info) {
         User user = userRepository.findByEmail(info.getEmail())
                 .orElseThrow(UserNotFoundException::new);
@@ -101,11 +108,13 @@ public class UserService {
                 .build();
     }
 
+    // 유저 상세 정보 조회
     public SignResponse getUser(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         return new SignResponse(user);
     }
 
+    // Refresh Token 생성
     public String createRefreshToken(User user) {
         Token token = tokenRepository.save(Token.builder()
                 .id(user.getId())
@@ -116,6 +125,7 @@ public class UserService {
         return token.getRefreshToken();
     }
 
+    // Refresh Token 검증
     public Token validRefreshToken(User user, String refreshToken) throws Exception {
         Token token = tokenRepository.findById(user.getId()).orElseThrow(TokenNotFoundException::new);
         if (token.getRefreshToken() == null) {
@@ -129,6 +139,7 @@ public class UserService {
         }
     }
 
+    // Access Token 갱신
     public TokenDto refreshAccessToken(TokenDto tokenDto) throws Exception {
         String email = jwtProvider.getEmail(tokenDto.getAccessToken());
         User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
@@ -140,6 +151,32 @@ public class UserService {
                     .build();
         } else {
             throw new Exception("로그인이 필요합니다.");
+        }
+    }
+
+    // 임시 비밀번호 발급 후 전송
+    @Transactional
+    public Boolean setToTempPassword(SendMailInfo sendMailInfo) {
+        String email = sendMailInfo.getEmail();
+        userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        String encodedTempPw = encoder.encode(mailService.sendTempPwMail(sendMailInfo));
+        try {
+            userRepository.updatePassword(email, encodedTempPw);
+            return true;
+        } catch (Exception e) {
+            throw new IllegalStateException("오류가 발생했습니다.");
+        }
+    }
+
+    // 비밀번호 변경
+    @Transactional
+    public Boolean changePassword(ChangePasswordInfo changePasswordInfo) throws IllegalAccessException {
+        User user = userRepository.findById(changePasswordInfo.getId()).orElseThrow(UserNotFoundException::new);
+        if (encoder.matches(changePasswordInfo.getOldPassword(), user.getPassword())) {
+            userRepository.updatePassword(user.getEmail(), encoder.encode(changePasswordInfo.getNewPassword()));
+            return true;
+        } else {
+            throw new IllegalAccessException("이전 비밀번호가 일치하지 않습니다.");
         }
     }
 }
