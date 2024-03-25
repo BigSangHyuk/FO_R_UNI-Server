368dd4727e6dc88d79e055b22415e5bcd418015e
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-25 11:17:57 +0900

    ♻️ 코드 수정: 특수문자 조합 변경

--- src/main/java/bigsanghyuk/four_uni/config/mail/service/MailService.java
+++ src/main/java/bigsanghyuk/four_uni/config/mail/service/MailService.java
@@ -88,7 +88,7 @@ public class MailService {
         char[] charSet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        char[] specialSet = new char[]{'~', '!', '@', '#', '$', '%', '^', '&', '*'};
+        char[] specialSet = new char[]{'!', '@', '#', '$', '%', '^', '+', '*', '=', '-'};
         StringBuilder str = new StringBuilder();
         int idx;
         for (int i = 0; i < 9; i++) {
