8c7480858b9ece7dac7e2ab16648df62b1c70402
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-28 15:16:54 +0900

    ✨ 기능 수정: 부모 댓글 삭제해도 자식 댓글 남아있게 soft delete 적용

--- src/main/java/bigsanghyuk/four_uni/comment/domain/entity/Comment.java
+++ src/main/java/bigsanghyuk/four_uni/comment/domain/entity/Comment.java
@@ -2,17 +2,18 @@ package bigsanghyuk.four_uni.comment.domain.entity;
 
 import bigsanghyuk.four_uni.comment.domain.EditCommentInfo;
 import bigsanghyuk.four_uni.config.domain.BaseTimeEntity;
-import bigsanghyuk.four_uni.post.domain.entity.Post;
 import bigsanghyuk.four_uni.user.domain.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity @Builder
 @Getter @Table(name = "comments")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE comments SET deleted = TRUE WHERE comment_id = ?") // soft delete 사용
 public class Comment extends BaseTimeEntity {
 
     @Id
@@ -40,6 +41,9 @@ public class Comment extends BaseTimeEntity {
     @ColumnDefault("0")
     @Setter
     private int commentReportCount;
+
+    @ColumnDefault("false")
+    private boolean deleted;
 
     public void edit(@Valid EditCommentInfo editCommentInfo) {
         this.content = editCommentInfo.getContent();
