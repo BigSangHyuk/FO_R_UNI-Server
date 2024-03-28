b789aa28d895b4f8cdbefb4f2378283f9b5a0db3
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-29 03:12:05 +0900

    ✨ 기능 수정: null인 필드 반환 x

--- src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
+++ src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
@@ -1,12 +1,14 @@
 package bigsanghyuk.four_uni.comment.dto;
 
 import bigsanghyuk.four_uni.user.dto.UserDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentDto {
 
     private Long commentId;
