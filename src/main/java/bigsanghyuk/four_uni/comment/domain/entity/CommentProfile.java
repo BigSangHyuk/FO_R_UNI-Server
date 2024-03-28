2d80dc6cd38b59fbbb4a10d161bdd6b178491b69
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-28 17:08:37 +0900

    ♻️ 코드 수정: 안쓰는 필드 삭제

--- src/main/java/bigsanghyuk/four_uni/comment/domain/entity/CommentProfile.java
+++ src/main/java/bigsanghyuk/four_uni/comment/domain/entity/CommentProfile.java
@@ -1,14 +1,10 @@
 package bigsanghyuk.four_uni.comment.domain.entity;
 
-import bigsanghyuk.four_uni.user.domain.entity.UserRequired;
-
 public interface CommentProfile {
 
     Long getCommentId();
 
     Long getUserId();
-
-    UserRequired getUser();
 
     Long getPostId();
 
