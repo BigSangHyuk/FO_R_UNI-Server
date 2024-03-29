6b5c39371f4dbe04e18a75e953b1f312c6e706b2
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-29 19:21:07 +0900

    ♻️ 코드 수정: UserDetails user 상속

--- src/main/java/bigsanghyuk/four_uni/config/jwt/CustomUserDetails.java
+++ src/main/java/bigsanghyuk/four_uni/config/jwt/CustomUserDetails.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails extends User implements UserDetails {
 
     private final User user;
 
