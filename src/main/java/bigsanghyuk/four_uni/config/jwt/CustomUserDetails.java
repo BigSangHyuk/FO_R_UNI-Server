534709bd9302c1ac33464386390b0788c879a658
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-03-29 21:56:08 +0900

    ♻️ 코드 수정: userDetail getter 수정

--- src/main/java/bigsanghyuk/four_uni/config/jwt/CustomUserDetails.java
+++ src/main/java/bigsanghyuk/four_uni/config/jwt/CustomUserDetails.java
@@ -34,7 +34,7 @@ public class CustomUserDetails extends User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getName();
+        return user.getEmail();
     }
 
     @Override
