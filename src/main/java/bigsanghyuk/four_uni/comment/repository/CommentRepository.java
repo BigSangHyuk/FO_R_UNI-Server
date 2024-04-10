f2e1d31405d133e6309d84ea588d3fbc85143663
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-10 16:11:21 +0900

    ♻️ 코드 수정: 레포지토리 메소드 변경

--- src/main/java/bigsanghyuk/four_uni/comment/repository/CommentRepository.java
+++ src/main/java/bigsanghyuk/four_uni/comment/repository/CommentRepository.java
@@ -4,10 +4,8 @@ import bigsanghyuk.four_uni.comment.domain.entity.Comment;
 import bigsanghyuk.four_uni.comment.domain.entity.CommentProfile;
 import bigsanghyuk.four_uni.comment.domain.entity.CommentRequired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,23 +18,13 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<CommentProfile> findCommentedPostId(@Param("userId") Long userId);
 
     @Query(nativeQuery = true,
-            value = "SELECT comment_id as commentId, user_id as userId, " +
-                    "comment_like as commentLike, content, deleted FROM comments " +
-                    "WHERE post_id = :postId AND parent_id = :parentId AND parent_id IS NOT NULL " +
-                    "ORDER BY created_at ASC"
-    )
-    List<CommentProfile> findChildComments(@Param("postId") Long postId, @Param("parentId") Long parentId);
-
-    @Query(nativeQuery = true,
-            value = "SELECT comment_id as commentId, user_id as userId, " +
-                    "comment_like as commentLike, content, deleted FROM comments " +
-                    "WHERE post_id = :postId AND parent_id IS NULL"
-    )
-    List<CommentProfile> findParentComments(@Param("postId") Long postId);
-
-    @Query(nativeQuery = true,
             value = "SELECT comment_id as commentId, user_id as userId, post_id as postId, " +
                     "comment_like as commentLike, content as content FROM comments " +
                     "WHERE comment_id = :commentId AND deleted = false")
     CommentRequired findCommentRequired(@Param("commentId") Long commentId);
+
+    @Query("SELECT c FROM Comment c WHERE c.postId = :postId AND c.parent IS NULL")
+    List<Comment> findParents(@Param("postId") Long postId);
+
+    List<Comment> findByParent(Comment parent);
 }
