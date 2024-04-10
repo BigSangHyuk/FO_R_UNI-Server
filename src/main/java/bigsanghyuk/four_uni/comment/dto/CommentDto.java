2794cd08cdff293e984e271dc860098914fee6b2
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-10 16:19:09 +0900

    ♻️ 코드 수정: dto 수정

--- src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
+++ src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
@@ -1,21 +1,33 @@
 package bigsanghyuk.four_uni.comment.dto;
 
+import bigsanghyuk.four_uni.comment.domain.entity.Comment;
 import bigsanghyuk.four_uni.user.dto.UserDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Builder
+@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentDto {
 
-    private Long commentId;
-    private Long userId;
+    private Long id;
     private UserDto user;
     private Integer commentLike;
     private String content;
     private Boolean isDeleted;
+    private Long parentId;
     private List<CommentDto> children;
+
+    public CommentDto(Comment comment) {
+        this.id = comment.getId();
+        this.user = comment.isDeleted() ? null : UserDto.of(comment.getUser());
+        this.commentLike = comment.isDeleted() ? null : comment.getCommentLike();
+        this.content = comment.isDeleted() ? null : comment.getContent();
+        this.isDeleted = comment.isDeleted() ? true : null;
+        this.parentId = comment.getParent() != null ? comment.getParent().getId() : null;
+        this.children = new ArrayList<>();
+    }
 }
