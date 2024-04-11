65f3ece418afbde7a2369544c740591d52f2de7a
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-11 10:52:25 +0900

    ♻️ 코드 수정: 필터 조건 추가

--- src/main/java/bigsanghyuk/four_uni/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/bigsanghyuk/four_uni/config/filter/JwtAuthenticationFilter.java
@@ -22,6 +22,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtProvider.resolveToken(request);
+        if (request.getRequestURI().equals("/refresh")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         if (token != null && jwtProvider.validateToken(token)) {
             token = token.split(" ")[1].trim();
             Authentication auth = jwtProvider.getAuthentication(token);
