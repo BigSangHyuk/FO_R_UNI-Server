2c515586fef47983044c5eb1f9118266876ac199
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-18 09:09:24 +0900

    ♻️ 코드 수정: setter 삭제

--- src/main/java/bigsanghyuk/four_uni/comment/domain/DeleteCommentInfo.java
+++ src/main/java/bigsanghyuk/four_uni/comment/domain/DeleteCommentInfo.java
@@ -2,10 +2,8 @@ package bigsanghyuk.four_uni.comment.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class DeleteCommentInfo {
 
--- src/main/java/bigsanghyuk/four_uni/comment/domain/EditCommentInfo.java
+++ src/main/java/bigsanghyuk/four_uni/comment/domain/EditCommentInfo.java
@@ -3,9 +3,8 @@ package bigsanghyuk.four_uni.comment.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EditCommentInfo {
