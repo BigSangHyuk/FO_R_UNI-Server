423483f9bbf798925db33cc247c0d7c1a54374e8
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-18 10:01:04 +0900

    🔥 코드 삭제: 이미지 업로드 컨트롤러 메소드 삭제 (userService 로 이동)

--- src/main/java/bigsanghyuk/four_uni/config/s3/controller/S3Controller.java
+++ src/main/java/bigsanghyuk/four_uni/config/s3/controller/S3Controller.java
@@ -1,20 +1,17 @@
 package bigsanghyuk.four_uni.config.s3.controller;
 
 import bigsanghyuk.four_uni.config.s3.dto.IcsUploadDto;
-import bigsanghyuk.four_uni.config.s3.dto.ImageUploadDto;
 import bigsanghyuk.four_uni.config.s3.service.S3Uploader;
 import bigsanghyuk.four_uni.post.service.ICalService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -27,18 +24,6 @@ public class S3Controller {
 
     private final S3Uploader s3Uploader;
     private final ICalService iCalService;
-
-    @Operation(summary = "이미지 업로드", description = "multipart/form-data 전송, 저장된 URL 반환")
-    @PostMapping(value = "/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> upload(@RequestParam(name = "images") MultipartFile multipartFile) {
-        try {
-            String uploadedUrl = s3Uploader.upload(multipartFile, "static");
-            return ResponseEntity.ok().body(new ImageUploadDto(uploadedUrl, "이미지 업로드에 성공했습니다."));
-        } catch (Exception e) {
-            log.error("Error occurred while uploading image: ", e);
-            return ResponseEntity.badRequest().body("잘못된 요청입니다.");
-        }
-    }
 
     @Operation(summary = "ics 업로드", description = "저장할 postId 입력하면 ics 저장된 url 반환")
     @PostMapping(value = "/ics")
