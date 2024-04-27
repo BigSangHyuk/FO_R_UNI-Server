f03106664c239f4eb1023a8dbdbeaa790da726fb
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-04-27 15:37:33 +0900

    ♻️ 코드 수정: 반환 값 추가, null 제외 반환

--- src/main/java/bigsanghyuk/four_uni/post/domain/entity/PostRequired.java
+++ src/main/java/bigsanghyuk/four_uni/post/domain/entity/PostRequired.java
@@ -1,13 +1,16 @@
 package bigsanghyuk.four_uni.post.domain.entity;
 
 import bigsanghyuk.four_uni.user.enums.CategoryType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.LocalDate;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public interface PostRequired {
 
     Long getPostId();
     CategoryType getCategory();
     String getTitle();
+    String getContent();
     LocalDate getDeadline();
 }
