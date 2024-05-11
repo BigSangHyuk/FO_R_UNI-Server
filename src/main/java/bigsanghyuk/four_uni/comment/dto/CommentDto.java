4895a5f282c8d01ee066920da21e8331db52a023
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-05-11 09:35:52 +0900

    ✨ 기능 추가: 댓글 반환에 좋아요 상태 컬럼 추가

--- src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
+++ src/main/java/bigsanghyuk/four_uni/comment/dto/CommentDto.java
@@ -3,12 +3,11 @@ package bigsanghyuk.four_uni.comment.dto;
 import bigsanghyuk.four_uni.comment.domain.entity.Comment;
 import bigsanghyuk.four_uni.user.dto.UserDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Getter
@@ -21,16 +20,18 @@ public class CommentDto {
     private String content;
     private Boolean isDeleted;
     private Long parentId;
+    private Boolean isLiked;
     private List<CommentDto> children;
     private LocalDateTime createdAt;
 
-    public CommentDto(Comment comment) {
+    public CommentDto(Comment comment, HashSet<Long> likedCommentIds) {
         this.id = comment.getId();
         this.user = comment.isDeleted() ? null : UserDto.of(comment.getUser());
         this.commentLike = comment.isDeleted() ? null : comment.getCommentLike();
         this.content = comment.isDeleted() ? null : comment.getContent();
         this.isDeleted = comment.isDeleted() ? true : null;
         this.parentId = comment.getParent() != null ? comment.getParent().getId() : null;
+        this.isLiked = likedCommentIds.contains(comment.getId());
         this.children = new ArrayList<>();
         this.createdAt = comment.getCreatedAt();
     }
