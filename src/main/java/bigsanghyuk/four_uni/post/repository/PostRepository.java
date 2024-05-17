f2db378cd3b221f362373670f957a5f1cd1c9a43
jae-kyeonggg <hanjaekyung56@gmail.com>
2024-05-17 17:30:48 +0900

    ✨ 기능 추가: 검색 메소드 최신순, 마감기한 순 정렬 기능

--- src/main/java/bigsanghyuk/four_uni/post/repository/PostRepository.java
+++ src/main/java/bigsanghyuk/four_uni/post/repository/PostRepository.java
@@ -45,4 +45,22 @@ public interface PostRepository extends JpaRepository<Post, Long> {
                     "ORDER BY created_at DESC"
     )
     List<PostRequired> findRequiredByKeyword(@Param("keyword") String keyword, @Param("classified") Boolean classified, @Param("category") List<String> categoryNames);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT post_id as postId, category, title, SUBSTRING(content, 1, 100) as content, deadline FROM posts " +
+                    "WHERE (title LIKE CONCAT('%', :keyword, '%') OR content LIKE CONCAT('%', :keyword, '%')) " +
+                    "AND deadline IS NOT NULL AND category in :category " +
+                    "ORDER BY created_at DESC"
+    )   // 최신순 정렬
+    List<PostRequired> findByKeywordLatest(@Param("keyword") String keyword, @Param("category") List<String> categoryNames);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT post_id as postId, category, title, deadline FROM posts " +
+                    "WHERE (title LIKE CONCAT('%', :keyword, '%') OR content LIKE CONCAT('%', :keyword, '%')) " +
+                    "AND deadline IS NOT NULL AND category in :category " +
+                    "ORDER BY CASE WHEN deadline >= CURDATE() THEN 0 ELSE 1 END, deadline, created_at"
+    )   // 조회일자 기준 마감기한 임박순으로 정렬하되, 마감기한이 남은 글들을 0번째 그룹, 이미 지난 글들을 1번째 그룹 -> 기간 남은 글들이 먼저 나오고 그 뒤에 마감된 글 표시
+    List<PostRequired> findByKeywordDeadline(@Param("keyword") String keyword, @Param("category") List<String> categoryNames);
 }
